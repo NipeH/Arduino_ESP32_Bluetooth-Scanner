ce357fad90233552d916f1d414732a54a6eaa0d6
Sam Kahrom <sam.kahrom@myy.haaga-helia.fi>
2020-09-30 17:30:30 +0300

    Navigaatio.js

--- App.js
+++ App.js
@@ -1,55 +1,7 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import {
-  BottomTabBar,
-  createBottomTabNavigator,
-} from "@react-navigation/bottom-tabs";
-
-import HomeView from "./HomeView";
-import DeviceView from "./DeviceView";
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
+import Navigation from "./Navigation";
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeView} />
-        <Tab.Screen name="Devices" component={DeviceView} />
-      </Tab.Navigator> */}
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeView}
-          options={{
-            headerShown: false,
-            title: " ",
-            headerStyle: {
-              backgroundColor: "#130c18",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Devices"
-          component={DeviceView}
-          options={{
-            title: "Recorded Devices",
-            headerStyle: {
-              backgroundColor: "#130c18",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigation/>
   );
 }
---
+++ Navigation.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+import HomeView from "./HomeView";
+import DeviceView from "./DeviceView";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      {/* <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeView} />
+        <Tab.Screen name="Devices" component={DeviceView} />
+      </Tab.Navigator> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeView}
+          options={{
+            headerShown: false,
+            title: " ",
+            headerStyle: {
+              backgroundColor: "#130c18",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Devices"
+          component={DeviceView}
+          options={{
+            title: "Recorded Devices",
+            headerStyle: {
+              backgroundColor: "#130c18",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
