d947f725a7e9d31a84d9626a17652297f81e35b6
Marco Brandt <mjt.brandt@gmail.com>
2020-10-08 09:58:24 +0300

    Pieniä muutoksia

--- DeviceView.js
+++ DeviceView.js
@@ -1,12 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
+
 
 export default function DeviceList() {
+  
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/person/all')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.text}>[LIST OF ALL RECORDED DEVICES]</Text>
       <StatusBar style="auto" />
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text style={{ color: 'blue' }}>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
     </View>
   );
 }
