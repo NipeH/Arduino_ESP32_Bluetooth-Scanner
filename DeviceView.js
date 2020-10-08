bc4cf6f2ac101984e4f8f4236a79514e81dd47ad
Marco Brandt <mjt.brandt@gmail.com>
2020-10-08 10:31:17 +0300

    Pieniä muutoksia

--- DeviceView.js
+++ DeviceView.js
@@ -9,23 +9,21 @@ export default function DeviceList() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8080/person/all')
+    fetch('https://reactnative.dev/movies.json')
       .then((response) => response.json())
-      .then((json) => setData(json))
+      .then((json) => setData(json.movies))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
   
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>[LIST OF ALL RECORDED DEVICES]</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
       {isLoading ? <ActivityIndicator/> : (
         <FlatList
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Text style={{ color: 'blue' }}>{item.title}, {item.releaseYear}</Text>
+            <Text>{item.title}, {item.releaseYear}</Text>
           )}
         />
       )}
