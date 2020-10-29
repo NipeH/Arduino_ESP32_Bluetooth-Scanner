8e891f169a61ff475852eb9b55f3cc1235441b96
KasperToikkanen <48006182+KasperToikkanen@users.noreply.github.com>
2020-10-29 10:03:59 +0200

    Update BLE_scan_esp32.ino
    mqtt kesken, rakennetta jäsennetty ja selkeytetty.

--- Esp32/BLE_scan_esp32.ino
+++ Esp32/BLE_scan_esp32.ino
@@ -1,21 +1,86 @@
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
 
+const char* ssid = "Wifi_verkkko";
+const char* password = "salasana";
+
+#include <PubSubClient.h>
+#define mqtt_server "http://localhost:1880/"
+WiFiClient espClient;
+PubSubClient client(espClient);
+//const char* mqttUser = "yourInstanceUsername";
+//const char* mqttPassword = "yourInstancePassword";
+#define haetutLaitteet "scannerMesh/laitteet"
+
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
 int scanTime = 10; //In seconds
 BLEScan* pBLEScan;
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.printf("ID: %s \n", advertisedDevice.getAddress().toString().c_str()); // Search result reduced for now to print only addresses
-    }
-};
 
 void setup() {
   Serial.begin(9600);
+  Serial.println();
+  
+  wifi_setup();
+  mqtt_setup();
+  
+}
+
+void wifi_setup(){
+  Serial.print("Yhdistetään verkkoon ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+
+  Serial.println("WiFi yhdistetty");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void mqtt_setup(){
+  client.setServer(mqtt_server, 1883);
+  client.connect("espScanner");
+
+  /*while (!client.connected()) {
+    Serial.println("Yhdistetään MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }*/
+  
+}
+
+void scannaus(){
+  
+  class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("ID: %s \n", advertisedDevice.getAddress().toString().c_str());
+    }
+  };
+  
   Serial.println("Scanning...");
 
   BLEDevice::init("");
@@ -24,14 +89,23 @@ void setup() {
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  //Serial.println("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  //Serial.println("Scan done!");
+  pBLEScan->clearResults();
+  delay(1000);
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.println("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-  delay(1000);
+  scannaus();
+  
+
+  client.publish(haetutLaitteet, String(BLEScanResults));
+
+  //StaticJsonBuffer<300> JSONbuffer;
+  //JsonObject& JSONencoder = JSONbuffer.createObject();
 }
