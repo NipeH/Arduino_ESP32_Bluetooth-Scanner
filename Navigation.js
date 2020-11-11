d990a3346a10c0d51f4d992ab59e452ccc061b0b
sammusama <kahrom.sam@gmai.com>
2020-11-11 14:17:29 +0200

    Tabi update

--- Navigation.js
+++ Navigation.js
@@ -5,6 +5,11 @@ import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
 import HomeView from "./HomeView";
 import DeviceView from "./DeviceView";
+import HistoryView from "./HistoryView";
+import StatsView from "./StatsView";
+import SettingsView from "./SettingsView";
+
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,31 +17,12 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      {/* <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeView} />
-        <Tab.Screen name="Devices" component={DeviceView} />
-      </Tab.Navigator> */}
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeView}
-          options={{
-            headerShown: false,
-            title: " ",
-            headerStyle: {
-              backgroundColor: "#130c18",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        />
-        <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name="Devices"
           component={DeviceView}
           options={{
-            title: "Recorded Devices",
+            title: "Devices",
             headerStyle: {
               backgroundColor: "#130c18",
             },
@@ -46,7 +32,52 @@ export default function Navigation() {
             },
           }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="History"
+          component={HistoryView}
+          options={{
+            headerShown: false,
+            title: "History",
+            headerStyle: {
+              backgroundColor: "#130c18",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+                <Tab.Screen
+          name="Stats"
+          component={StatsView}
+          options={{
+            headerShown: false,
+            title: "Statistics",
+            headerStyle: {
+              backgroundColor: "#130c18",
+            },
+            headerTintColor: "red",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+                <Tab.Screen
+          name="Settings"
+          component={SettingsView}
+          options={{
+            headerShown: false,
+            title: "Settings",
+            headerStyle: {
+              backgroundColor: "#130c18",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
